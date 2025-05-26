0f57f1f7587e53de854dc025552671a2704d0ea3
Navin Nishanth <68501003+navinniish@users.noreply.github.com>
2025-05-26 23:24:32 +0530

    Update __init__.py

--- pybleau/__init__.py
+++ pybleau/__init__.py
@@ -3,4 +3,8 @@ pybleau - A Python library for Tableau REST and Metadata APIs
 """
 
 __version__ = "0.1.0"
-__author__ = "navinniish"
+
+# Import main classes for easier access
+from .auth import TableauClient
+
+__all__ = ['TableauClient']
